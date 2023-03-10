e6206a67148d2f2ed79b4e77234b5fed09142fc4
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-03-10 13:02:49 +0100

    HOW TO LOAD MODEL

---
+++ load.py
@@ -0,0 +1,15 @@
+"""HOW TO LOAD MODEL"""
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+
+music_data = pd.read_csv('music.csv')
+x = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+joblib.dump(model, 'music-recommender.joblib')
+# predictions = model.predict( [21, 1], [22, 0] ) 
